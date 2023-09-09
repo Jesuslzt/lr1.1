7de462ed59da50540369e61bea19c81288fbd4ee
Jesuslzt <96657265+Jesuslzt@users.noreply.github.com>
2023-09-10 02:42:36 +0300

    asfasf

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println("Слабоспицький Олександр ПЗ-20-1/9 ");
+        System.out.println("Новая строчка ");
+    }
+}
