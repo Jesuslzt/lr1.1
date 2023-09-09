3b952aa62848be5d25f5cec50e3bf826b8d9a5b7
Jesuslzt <96657265+Jesuslzt@users.noreply.github.com>
2023-09-10 02:46:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Слабоспицький Олександр ПЗ-20-1/9 ");
-        System.out.println("Новая строчка ");
+        System.out.println("Новая строчка "); // новая строчка
     }
 }
